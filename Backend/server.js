95bc30d9cb8453c90673d31a1a00d7210554a19c
LuanKitsune <hagllerll@gmail.com>
2024-06-18 22:57:54 -0300

    material para nivelamento

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const participantRoutes = require("./routes/participantRoutes");
+const errorHandler = require("./utils/errorHandler");
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Bem vindo" });
+});
+
+app.use("/auth", authRoutes);
+app.use("/events", eventRoutes);
+app.use("/participants", participantRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
